312dc58a7045ddbe24efeeb6cce5f1b8d48a0ae0
Junu <bigdwarf_@naver.com>
2024-10-30 09:18:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ let text = `## <p align="center">🚀 About Me</p>
     <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" />
     <img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" />
     <img src="https://img.shields.io/badge/jQuery-0769AD?style=for-the-badge&logo=jquery&logoColor=white" />
+    <img src="https://img.shields.io/badge/Docker-2496ED?style=for-the-badge&logo=docker&logoColor=white" />
 </p>
 
 ## <p align="center">🔗 Links</p>
