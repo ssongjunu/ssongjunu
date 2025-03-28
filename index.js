94bc141936f46ac15ae4cf371ffff2add7caffa1
junu <bigdwarf_@naver.com>
2025-03-28 13:20:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -34,10 +34,6 @@ let text = `## <p align="center">🚀 About Me</p>
     <a href="mailto:bigdwarf_@naver.com"><img src="https://img.shields.io/badge/Email-D14836?style=for-the-badge&logo=gmail&logoColor=white" /></a>
 </p>
 
-<p align="center">
-  <a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fssongjunu%2Fhit-counter&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false"/></a>
-</p>
-
 ## <p align="center">📕 Latest Blog Posts</p>
 
 `;
